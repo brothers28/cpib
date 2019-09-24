04041ca1d558496ed51e4c67dc58b6919e60b33d
benjamin.meyer2 <benjamin.meyer2@students.fhnw.ch>
2019-09-24 17:26:44 +0200

    Initial commit

---
+++ src/main/java/ch/fhnw/edu/cpib/cpib/CpibApplication.java
@@ -0,0 +1,13 @@
+package ch.fhnw.edu.cpib.cpib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpibApplication.class, args);
+	}
+
+}
