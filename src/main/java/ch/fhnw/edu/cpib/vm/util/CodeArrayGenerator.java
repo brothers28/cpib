f43ad432526b4d05c53b09a944d80fc98ecb72bc
Marco Romanutti <marco.romanutti@students.fhnw.ch>
2019-12-27 20:26:46 +0100

    Code refactoring

--- src/main/java/ch/fhnw/edu/cpib/vm/util/CodeArrayGenerator.java
+++ src/main/java/ch/fhnw/edu/cpib/vm/util/CodeArrayGenerator.java
@@ -4,6 +4,7 @@ import ch.fhnw.edu.cpib.ast.AstTree;
 import ch.fhnw.edu.cpib.vm.ICodeArray;
 
 public class CodeArrayGenerator {
+    // TODO: Braucht es diese Klasse?! Kann ja auch direkt auf as ausgeführt werden...
     public ICodeArray convert(AstTree as) throws ICodeArray.CodeTooSmallError {
         return as.getCodeArray();
     }
